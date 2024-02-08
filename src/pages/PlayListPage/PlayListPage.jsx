219ea44a5a8d04c310062427b3f306943f562d13
Evgeny Salagin <114258961+Erksery@users.noreply.github.com>
2024-02-08 11:19:29 +0300

    v.0.1.7

--- src/pages/PlayListPage/PlayListPage.jsx
+++ src/pages/PlayListPage/PlayListPage.jsx
@@ -13,12 +13,12 @@ function PlayListPage(props) {
 
     const context = canvas.getContext("2d");
     const image = new Image();
-    image.src = "http://localhost:5007/uploads/1707299461255.394576319.jpg";
+    image.src = "http://localhost:5007/uploads/1707374319094.894161516.png";
     image.onload = () => {
       canvas.width = image.width;
       canvas.height = image.height;
       context.beginPath();
-      context.rect(100, 50, 200, 200);
+      context.rect(100, 170, 200, 200);
       context.clip();
       context.drawImage(image, 0, 0);
     };
@@ -26,30 +26,25 @@ function PlayListPage(props) {
     setCanvasImageData(croppedImageData);
   }, []);
 
-  const sendFile = async () => {
-    try {
-    } catch (err) {
-      console.log("Error", err);
-    }
-  };
 
-  //непонятно
+
   const handleUploadImage = async () => {
-    let formData = new FormData();
+    const formData = new FormData();
     formData.append("file", canvasImageData);
-    try {
-      await axios.post("/api/fileUpload", formData, {
-        headers: { "Content-Type": "multipart/form-data" },
-      });
-      console.log(...formData);
-    } catch (err) {
-      alert(err);
-    }
+      try {
+        await axios.post("/api/circumcisedImage", {image: formData.get("file")});
+        console.log(imageData);
+      } catch (err) {
+        alert(err);
+        console.log(...formData);
+      }
   };
 
   return (
     <BodyContainer>
-      <button onClick={handleUploadImage}>Upload</button>
+      <button contentEditable="true"
+      //  onClick={handleUploadImage}
+       >Upload</button>
       <canvas ref={canvasRef} />
     </BodyContainer>
   );
